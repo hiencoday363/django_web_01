4109ff2b8eb3061050db03eae9ecec100bf6404d
hiencoday363 <giaphien363616@gmail.com>
2021-07-03 08:54:46 +0700

    comment db in settting.py

--- scrap_chatbot_django/settings.py
+++ scrap_chatbot_django/settings.py
@@ -103,6 +103,7 @@ DATABASES = {
 }
 """
 # using postgresql
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -113,6 +114,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
